28f71c445f92b94695357b562f87f074bbfc708b
Jesus-Montesinos <fredturnerr31@gmail.com>
2023-07-04 10:55:44 -0600

    second commit

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -1,8 +1,4 @@
 'use client'
-import { usePathname } from 'next/navigation';
-
-export default function Movie() {
-  const pathname = usePathname();
-  const slug = pathname.substring(7); 
-    return <h1>Hello, MOVIES details {slug}</h1>
+export default function Movie({params}: any) {
+    return <h1>Hello, MOVIES details {params.id}</h1>
   }
